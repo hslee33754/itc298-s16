88649ea2d9053e164993544b7ab434dd80f9a94d
Kate <hslee.33754@gmail.com>
2016-04-06 03:15:58 +0000

    Added ServeStatic Function

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 var http = require("http"), fs = require("fs");
 
+function serveStatic(res, path, contetnType, responseCode){
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err,data){
+        if(err) {
+            res.writeHead(500, {'Content-Type' : 'text/plain'});
+            res.end('500-Internal Error');
+        }else{
+            res.writeHead(responseCode, {'Content-Type' : contetnType});
+            res.end(data);
+        }
+    })
+}
+
 http.createServer(function(req,res){
-    console.log("test1")
-    res.writeHead(200, {'Content-Type' : 'text/plain' });
-    res.end('Aloha World');
+    var path = req.url.toLocaleLowerCase();
+    
+    switch (path) {
+        case '/':
+            //res.writeHead(200, {'Content-Type': 'text/plain'});
+            //res.end('Home page');
+            serveStatic(res,'/public/home.html', 'text/html');
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-Type' : 'text/plain' });
+            res.end('About page');
+            break;
+        default:
+            res.writeHead(404, {'Content-Type' : 'text/plain'});
+            res.end('Not Found');
+            break;
+    }
     
 }).listen(process.env.PORT);
-console.log("test2")
